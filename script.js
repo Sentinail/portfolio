02591d030630fb777090f8a34157e9370814d670
Sentinail <wilsongponseca@gmail.com>
2023-05-20 17:39:01 +0800

    added intersection observer

--- script.js
+++ script.js
@@ -12,3 +12,32 @@ for (let i = 0; i < portfolioImgs.length; i++) {
         modalImg.src = portfolioImgs.item(i).children[0].src
     }
 }
+
+// Intersection Animation
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            if (entry.target.classList.contains("hidden")) {
+                entry.target.classList.add("show");
+            }
+
+            if (entry.target.classList.contains("top-bottom") || entry.target.classList.contains("left-right") || entry.target.classList.contains("right-left")) {
+                entry.target.classList.add("slide");
+            }
+        }
+
+        // else {
+        //     entry.target.classList.remove("show")
+        //     entry.target.classList.remove("slide");
+        // }
+    })
+})
+
+let hidden = document.querySelectorAll(".hidden");
+let leftRight = document.querySelectorAll(".left-right");
+let rightLeft = document.querySelectorAll(".right-left");
+let topBottom = document.querySelectorAll(".top-bottom");
+hidden.forEach((el) => observer.observe(el));
+leftRight.forEach((el) => observer.observe(el));
+rightLeft.forEach((el) => observer.observe(el));
+topBottom.forEach((el) => observer.observe(el));
